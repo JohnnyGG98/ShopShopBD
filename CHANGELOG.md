c3adfed3af8112653ef310b9f4f4198a4ed8fe98
Lina Orellana <linis_meche44@hotmail.com.ar>
2019-09-03 13:59:09 -0500

    Corrigiendo pequeños errores en Producto.sql

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,28 @@ Se agrego una columna, para poder guardar el numero de productos que vendio.
 ALTER TABLE "vendedores" ADD COLUMN "vend_general"
 TYPE INT NOT NULL DEFAULT '0';
 ```
+
+**03/09/2019**
+
+Se corrigió un error al momento de ejecutar el script en la BDShopShop, faltaba una coma ,
+
+
+```sql
+  prod_activo BOOLEAN DEFAULT 'true',
+  CONSTRAINT productos_pk PRIMARY KEY ("id_producto")
+) WITH (OIDS = FALSE);
+```
+
+Se corrigió un error, faltaba agregar el with(oids=false);
+
+```sql
+  prst_fecha_ingreso TIMESTAMP NOT NULL DEFAULT now(),
+  CONSTRAINT producto_stock_pk PRIMARY KEY("id_producto_stock")
+)WITH (OIDS = FALSE);
+```
+
+Se corrigió error en la fecha de ingreso de la tabla Productos
+
+```sql
+prod_fecha_ingreso TIMESTAMP DEFAULT now(),
+```
--- Tablas/Producto.sql
+++ Tablas/Producto.sql
@@ -1,69 +1,88 @@
-CREATE TABLE "Productos"(
+CREATE TABLE "Productos"
+(
   id_producto BIGSERIAL NOT NULL,
   id_vendedor BIGINT NOT NULL,
   id_marcar BIGINT NOT NULL,
   prod_nombre CHARACTER VARYING(255) NOT NULL,
-  prod_fecha_ingreso TIMESTAMP DEFAULT, 'SN'
+  prod_fecha_ingreso TIMESTAMP DEFAULT now(),
   prod_stock_total int NOT NULL,
   prod_marca CHARACTER VARYING(50) NOT NULL,
   prod_precio_venta numeric NOT NULL,
   prod_descripcion TEXT NOT NULL,
-  prod_restriccion_edad_max  int NOT NULL,
+  prod_restriccion_edad_max int NOT NULL,
   prod_restriccion_edad_min int NOT NULL,
-  prod_activo BOOLEAN DEFAULT 'true'
-  CONSTRAINT productos_pk PRIMARY KEY ("id_productos")
-) WITH (OIDS = FALSE);
+  prod_activo BOOLEAN DEFAULT 'true',
+  CONSTRAINT productos_pk PRIMARY KEY ("id_producto")
+)
+WITH (OIDS = FALSE);
 
-CREATE TABLE "ProductosCategorias"(
+CREATE TABLE "ProductosCategorias"
+(
   id_producto_categoria BIGSERIAL NOT NULL,
   id_producto BIGINT NOT NULL,
   id_categoria BIGINT NOT NULL,
   prca_activo BOOLEAN DEFAULT 'true',
   CONSTRAINT producto_categoria_pk PRIMARY KEY("id_producto_categoria")
-) WITH (OIDS = FALSE);
+)
+WITH (OIDS = FALSE);
 
-CREATE TABLE "Marcas"(
+CREATE TABLE "Marcas"
+(
   id_marca BIGSERIAL NOT NULL,
   marc_nombre character varying(150) NOT NULL,
   marc_codigo character varying(50) NOT NULL,
   marc_actvio BOOLEAN DEFAULT 'true',
   CONSTRAINT marca_pl PRIMARY KEY("id_marca")
-) WITH (OIDS = FALSE);
+)
+WITH (OIDS = FALSE);
 
-CREATE TABLE "ProductosStock" (
+CREATE TABLE "ProductosStock"
+(
   id_producto_stock BIGSERIAL NOT NULL,
   id_producto BIGINT NOT NULL,
   prst_cantidad INT NOT NULL,
   prst_fecha_ingreso TIMESTAMP NOT NULL DEFAULT now(),
   CONSTRAINT producto_stock_pk PRIMARY KEY("id_producto_stock")
 )
+WITH (OIDS = FALSE);
 
-CREATE TABLE "Imagenes"(
+CREATE TABLE "Imagenes"
+(
   id_imagen BIGSERIAL NOT NULL,
   id_producto BIGINT NOT NULL,
   ima_activo BOOLEAN DEFAULT 'true',
   CONSTRAINT imagen_pk PRIMARY KEY ("id_imagen")
-) WITH (OIDS = FALSE);
+)
+WITH (OIDS = FALSE);
 
-CREATE TABLE "Categorias"(
+CREATE TABLE "Categorias"
+(
   id_categoria BIGSERIAL NOT NULL,
   cat_nombre character varying(50) NOT NULL,
   cat_codigo character varying(50) NOT NULL,
   cat_activo BOOLEAN DEFAULT 'true',
   CONSTRAINT categoria_pk PRIMARY KEY ("id_categoria")
-) WITH (OIDS = FALSE);
+)
+WITH (OIDS = FALSE);
 
-CREATE TABLE "Unidades" (
+CREATE TABLE "Unidades"
+(
   id_unidad BIGSERIAL NOT NULL,
   unid_nombre character varying(100) NOT NULL,
   unid_codigo character varying(10) NOT NULL,
   CONSTRAINT unidad_pk PRIMARY KEY ("id_unidad")
-) WITH (OIDS = FALSE);
+)
+WITH (OIDS = FALSE);
 
-CREATE TABLE "Comentarios" (
+CREATE TABLE "Comentarios"
+(
   id_comentario BIGSERIAL NOT NULL,
   id_producto BIGINT NOT NULL,
   comentario character varying(255),
   com_activo BOOLEAN DEFAULT 'true',
   CONSTRAINT comentario_pk PRIMARY KEY ("id_comentario")
-) WITH (OIDS = FALSE);
+)
+WITH (OIDS = FALSE);
+
+
+
